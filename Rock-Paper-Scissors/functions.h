d1e8ed825a6657342e044cd7ef95ac2d53c95764
delacrank <jcarias86@gmail.com>
2016-09-07 19:38:26 -0400

    Update functions.h

--- Rock-Paper-Scissors/functions.h
+++ Rock-Paper-Scissors/functions.h
@@ -6,18 +6,34 @@
 
 using namespace std;
 
+// store life in this variable
 extern int life;
 extern int w;
 
+// take player input
 int input();
+
+// computer random number generator
 int CompAI();
+
+// display game over
 int gameover();
+
+// function for each selection of rock paper scissors
 void rock();
 void paper();
 void scissors();
+
+// error display
 void default1();
+
+// main game function
 void selection();
+
+// display game options
 void info();
+
+// check to see that you have enough life to keep playing
 void check();
 
 #endif
