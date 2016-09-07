33a48bd1b6678895c8112b27d6c6b35202d1a10d
delacrank <jcarias86@gmail.com>
2016-09-07 19:34:49 -0400

    Functions header

---
+++ Rock-Paper-Scissors/functions.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+extern int life;
+extern int w;
+
+int input();
+int CompAI();
+int gameover();
+void rock();
+void paper();
+void scissors();
+void default1();
+void selection();
+void info();
+void check();
+
+#endif
